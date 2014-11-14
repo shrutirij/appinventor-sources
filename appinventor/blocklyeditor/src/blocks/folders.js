2f2284d5d647774dbffdd19c895ed01bec87b9a6
Shirley Lu <xlu2@wellesley.edu>
2014-11-14 10:30:03 -0500

    fixed typo

--- appinventor/blocklyeditor/src/blocks/folders.js
+++ appinventor/blocklyeditor/src/blocks/folders.js
@@ -5,7 +5,7 @@ goog.provide('Blockly.Blocks.folder');
 Blockly.Blocks['folder'] = {
     category: "Folders",
     init: function() {
-        this.setColour(Blockly.FOLDER_CATEGORY_HUES);
+        this.setColour(Blockly.FOLDER_CATEGORY_HUE);
         this.appendDummyInput()
             .appendField(new Blockly.FieldTextBlockInput('FOLDER NAME'), 'TEXT');
     },
