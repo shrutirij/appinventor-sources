f7c54def8b8854f61d6b66aa568580465c0f5cd0
shirleylu <xlu2@wellesley.edu>
2015-03-22 00:27:49 -0400

    taking out some console logs

--- appinventor/lib/blockly/src/core/block.js
+++ appinventor/lib/blockly/src/core/block.js
@@ -250,8 +250,6 @@ Blockly.Block.prototype.initSvg = function() {
     Blockly.bindEvent_(this.svg_.getRootElement(), 'mousedown', this,
                        this.onMouseDown_);
   }
-    console.log(this.workspace);
-    console.log(this.workspace.getCanvas());
   this.workspace.getCanvas().appendChild(this.svg_.getRootElement());
 };
 
